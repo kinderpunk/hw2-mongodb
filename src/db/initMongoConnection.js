aacc87ad3a2ddc08f258a1901d4a184492afb7f1
kinderpunk <tsymbal.vladislav@gmail.com>
2024-09-08 14:56:39 +0200

    qwe

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,7 +9,8 @@ export async function initMongoConnection() {
   console.log('MONGODB_URL:', MONGODB_URL);
   console.log('MONGODB_DB:', MONGODB_DB);
 
-  const uri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
 
   try {
     await mongoose.connect(uri, {
